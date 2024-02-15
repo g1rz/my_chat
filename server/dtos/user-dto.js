b9f070fb7a1079d6fb1fcefad308e7acbb8f0efb
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-02-15 09:52:17 +0800

    18263 Выносит сервер в отдельный репозиторий

--- server/dtos/user-dto.js
+++
@@ -1,7 +0,0 @@
-export default class UserDto {
-	constructor(model) {
-		this.email = model.email;
-		this.id = model._id;
-		this.isActivated = model.isActivated;
-	}
-}
--- server/models/channel-model.js
+++
@@ -1,7 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const ChannelSchema = new Schema({
-    name: {type: String, unique: true, required: true},
-});
-
-export default model('Channel', ChannelSchema);
